cb82b307e18bcc3a717a334efcca57765b38dc46
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2025-07-22 14:56:58 -0600

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import KitchenInventoryForm from './components/KitchenInventoryForm'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Formularios - Café Playa Negra</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, dignissimos?</p>
+      <hr />
+      <KitchenInventoryForm/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/KitchenInventoryForm.tsx
@@ -0,0 +1,12 @@
+const KitchenInventoryForm = () => {
+
+  return (
+    <div>
+        <h1>Formulario 1: Inventario Cocina - Conteo</h1>
+      <p>Este inventario es para Lorem ipsum dolor sit amet.</p>
+      
+    </div>
+  )
+}
+
+export default KitchenInventoryForm
