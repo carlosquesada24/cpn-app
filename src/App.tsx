b89b9ba6464d8d67e8152bb9ad6b6654bb5c83d6
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2025-07-28 15:33:47 -0600

    Simple products list data created and rendered on Pending products to count view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import KitchenInventoryForm from './components/KitchenInventoryForm'
+import { PRODUCTS_LIST } from './data'
 
 function App() {
 
@@ -8,6 +9,13 @@ function App() {
       
       <h1 className='text-2xl font-bold'>Formularios - Café Playa Negra</h1>
       <KitchenInventoryForm/>
+      <hr />
+      <h1 className='text-2xl font-bold'>Productos pendientes</h1>
+      <ul>
+        {
+          PRODUCTS_LIST.map(product => <li>{product}</li>)
+        }
+      </ul>
     </>
   )
 }
