b3451de4d1cfd7e8e5071b974de9d255ff704709
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2025-07-30 16:29:57 -0600

    Header added on all routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import Header from "./components/Header.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
+  <Header/>
     <StrictMode>
       <Routes>
         <Route index element={<App />} />
