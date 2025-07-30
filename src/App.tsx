24a9efae3287efb29c51864a7a698794f1554491
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2025-07-30 16:31:16 -0600

    Unused import deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import Header from './components/Header'
 import KitchenInventoryForm from './components/KitchenInventoryForm'
 
 function App() {
 
   return (
     <>    
-    
       <KitchenInventoryForm/>
     </>
   )
