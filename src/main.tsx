48a40cd0cd51e43c866aaf4208c38b0d1b5e2437
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2025-07-30 16:09:29 -0600

    Inventory path added

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-   <BrowserRouter>
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
     <StrictMode>
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
-  </StrictMode>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="/inventory" element={<h1>Inventarios</h1>} />
+      </Routes>
+    </StrictMode>
   </BrowserRouter>
-  ,
-)
+);
