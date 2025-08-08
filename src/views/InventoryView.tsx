179b037dd7ae7dc58a24d76cde9f59705a57c6c3
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2025-08-08 17:35:50 -0600

    getAllProducts fetched from Inventory view

--- src/views/InventoryView.tsx
+++ src/views/InventoryView.tsx
@@ -1,11 +1,31 @@
+import { Link } from "react-router"
 import { InventoryTable } from "../components/InventoryTable"
 import { PRODUCTS_LIST } from "../data"
+import supabase from "../utils/supabase";
+import { useEffect } from "react";
 
 const InventoryView = () => {
+
+const getAllProducts = async() =>  {
+    const { data, error } = await supabase.from("Products").select(`
+        name
+        `);
+
+    if (error) {
+        console.log({ error });
+    }
+
+    console.log({ data, error});
+};
+
+useEffect(()=> {
+  getAllProducts()
+})
+
   return (
     <div>
         <h1 className='text-2xl font-bold mb-4'>Inventarios</h1>
-
+        
     
         <InventoryTable inventoryProductsList={PRODUCTS_LIST}/>
     </div>
