085f7d1a12835eff4e5c6133ae77cee1bf06c635
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2025-08-10 05:41:15 -0600

    Table item actions dropdown toggle created

--- src/components/InventoryTable.tsx
+++ src/components/InventoryTable.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router";
 import { PRODUCT_TYPES, type Product } from "../data";
+import { useState } from "react";
 
 type InventoryTableProps = {
     inventoryProductsList: Product[]
@@ -16,6 +17,14 @@ const BADGE_STYLES = {
 }
 
 export function InventoryTable({inventoryProductsList}: InventoryTableProps) {
+
+const [openDropdownId, setOpenDropdownId] = useState(null);
+
+  const toggleDropdown = (id: any) => {
+    setOpenDropdownId((prev) => (prev === id ? null : id));
+  };
+
+
   return (
     <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
             <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
@@ -115,24 +124,29 @@ export function InventoryTable({inventoryProductsList}: InventoryTableProps) {
                             <td className="px-4 py-3">{item.category}</td>
   
                             <td className="px-4 py-3 flex items-center justify-end">
-                                <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown" className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
+                                <button id="apple-imac-27-dropdown-button" onClick={() => toggleDropdown(item.id)} data-dropdown-toggle="apple-imac-27-dropdown" className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
                                     <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                     </svg>
                                 </button>
-                                <div id="apple-imac-27-dropdown" className="z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-                                    <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="apple-imac-27-dropdown-button">
-                                        <li>
-                                            <Link to={"/inventory-count-form/" + item.id} className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Contar</Link>
-                                        </li>
-                                        <li>
-                                            <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Editar</a>
-                                        </li>
-                                    </ul>
-                                    <div className="py-1">
-                                        <a href="#" className="block py-2 px-4 text-sm bg-red-600 text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Eliminar</a>
+
+                                {openDropdownId === item.id && (
+                                    <div id="apple-imac-27-dropdown" className=" z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                                        <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="apple-imac-27-dropdown-button">
+                                            <li>
+                                                <Link to={"/inventory-count-form/" + item.id} className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Contar</Link>
+                                            </li>
+                                            <li>
+                                                <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Editar</a>
+                                            </li>
+                                        </ul>
+                                        <div className="py-1">
+                                            <a href="#" className="block py-2 px-4 text-sm bg-red-600 text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Eliminar</a>
+                                        </div>
                                     </div>
-                                </div>
+                                )}
+
+                                
                             </td>
                         </tr>)
                         }
