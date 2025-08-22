fb40a8f38a5ca9c268c3b79bc082a67a5352d395
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2025-08-22 05:13:07 -0600

    OrdersCard UI fixed

--- src/views/OrdersView.tsx
+++ src/views/OrdersView.tsx
@@ -3,33 +3,29 @@ import { useEffect, useState } from "react";
 type CardProps = {
     order: {
         name: string
+        state: number
     };
 }
 
-const Card = ({order}: CardProps) => {
+const ORDER_STATES = {
+  1: "Pendiente de enviar",
+  2: "Enviada"
+}
+
+const OrderCard = ({order}: CardProps) => {
     return (
          <div className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
-        <div className="h-56 w-full">
-          <a href="#">
-            <img className="mx-auto h-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt="" />
-            <img className="mx-auto hidden h-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg" alt="" />
-          </a>
-        </div>
-        <div className="pt-6">
 
           <a href="#" className="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">{order.name}</a>
 
           <div className="mt-4 flex items-center justify-between gap-4">
-            <p className="text-2xl font-extrabold leading-tight text-gray-900 dark:text-white">Enviada</p>
+            {/* Estado */}
+            <p className="text-2xl font-extrabold leading-tight text-gray-900 dark:text-white">{ORDER_STATES[order.state]}</p>
 
             <button type="button" className="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4  focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-              <svg className="-ms-2 me-2 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
-              </svg>
-              Add to cart
+              Ver más
             </button>
           </div>
-        </div>
       </div>
     )
 }
@@ -54,9 +50,9 @@ const OrdersView = () => {
 
 
 <div className="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
-      <Card order={{name: "Orden 1 - 1° semana Agosto"}} />
-     <Card order={{name: "orden 2"}}/>
-       <Card order={{name: "orden 3"}}/>
+      <OrderCard order={{name: "Orden 1 - 2° semana Agosto", state: 1}} />
+     <OrderCard order={{name: "Orden 1 - 1° semana Agosto", state: 2}}/>
+       <OrderCard order={{name: "Orden 4 - 4° semana Julio", state: 2}}/>
   
     </div>
     </div>
